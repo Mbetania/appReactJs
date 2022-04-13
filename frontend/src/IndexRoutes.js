b014f23a662e25acc89f4588a0f63ddc917d00ec
ivanberen <ivan.beren.mdp@gmail.com>
2022-04-12 23:10:24 -0300

    update

---
+++ frontend/src/IndexRoutes.js
@@ -0,0 +1,13 @@
+import { Routes, Route, Link } from "react-router-dom";
+import React from 'react'
+
+const IndexRoutes = () => {
+return (
+  <>
+    IndexRoutes
+  </>
+)
+}
+
+export default IndexRoutes
+
---
+++ frontend/src/routes/AppRoutes.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import {Col,Row} from 'react-bootstrap'
+import '../styles/App.css';
+
+const AppRoutes = () => {
+    const [value, setValue] = useState(0)
+
+    const handleChange = () => {
+        setValue(value+1)
+    }
+
+    useEffect(() => {
+        console.log('value: ',value)
+    }, [value])
+    
+
+
+    return (
+        <>
+            <Row className='rowTamanio'>
+                <Col className="rowColor" xs={4}>
+                </Col>
+                <Col className="rowColorSecond" xs={8}>
+                </Col>
+            </Row>
+        
+        </>
+    );
+};
+
+export default AppRoutes;
